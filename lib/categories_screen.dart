0ae40ac7076f3e56579b1432a18d50f13fe7d424
amitkum8227 <62775823+amitkum8227@users.noreply.github.com>
2021-07-16 03:06:33 -0400

    "gridview created"

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-
+import 'dummy_data.dart';
+import 'category_item.dart';
 class CatergoriesScreen extends StatelessWidget {
-  const CatergoriesScreen({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return Scaffold(
+      appBar: AppBar(title: Text('FoodieMe'),),
+      body:GridView(
+        children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
-            mainAxisSpacing: 20));
+            mainAxisSpacing: 20)) ,) ;
   }
 }
